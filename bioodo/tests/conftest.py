ca73bfd4c5552b3e3d1389367d959053ac8abc3e
Per Unneberg <per.unneberg@scilifelab.se>
2016-11-17 23:28:04 +0100

    Correct conftest layout

--- bioodo/tests/conftest.py
+++ bioodo/tests/conftest.py
@@ -16,24 +16,6 @@ BIOODO = join(TESTDIR, os.pardir, os.pardir, "bioodo")
 
 blacklist = ['__pycache__', '__init__.py', 'tests', '_version.py', 'pandas.py', 'utils.py']
 applications = [x.strip('.py') for x in os.listdir(BIOODO) if x not in blacklist]
-
-# Custom options
-def pytest_addoption(parser):
-    parser.addoption("--slow", action="store_true",
-        help="run slow tests")
-    parser.addoption("-P", "--python2-conda", action="store",
-                     default="py2.7",
-                     help="name of python2 conda environment [default: py2.7]",
-                     dest="python2_conda")
-    parser.addoption("-A", "--application", action="store",
-                     help="application to test",
-                     dest="application")
-    parser.addoption("-T", "--threads", action="store",
-                     default="1",
-                     help="number of threads to use",
-                     dest="threads")
-
-
     
 # Add namespace with test files and director
 def pytest_namespace():
