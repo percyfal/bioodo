b0d6021eafb74feef6521175432d227783c562ca
Per Unneberg <per.unneberg@scilifelab.se>
2017-05-16 13:48:21 +0200

    Update cutadapt for pytest_ngsfixtures

--- bioodo/tests/test_cutadapt.py
+++ bioodo/tests/test_cutadapt.py
@@ -1,28 +1,18 @@
 # Copyright (C) 2015 by Per Unneberg
-import os
-import pytest
 from bioodo import cutadapt, odo, DataFrame
 
+from pytest_ngsfixtures.config import application_fixtures
+import utils
 
-@pytest.fixture(scope="module")
-def cutadapt_se_data(tmpdir_factory):
-    fn = tmpdir_factory.mktemp('cutadapt').join('single_end.cutadapt_metrics')
-    fn.mksymlinkto(os.path.join(pytest.datadir, "cutadapt", "s1_1.trimmed.fastq.gz.cutadapt_metrics"))
-    return fn
+fixtures = application_fixtures(application="cutadapt")
+cutadapt_metrics = utils.fixture_factory(fixtures)
 
-@pytest.fixture(scope="module")
-def cutadapt_pe_data(tmpdir_factory):
-    fn = tmpdir_factory.mktemp('cutadapt').join('paired_end.cutadapt_metrics')
-    fn.mksymlinkto(os.path.join(pytest.datadir, "cutadapt", "s1.fastq.gz.cutadapt_metrics"))
-    return fn
 
-             
-def test_cutadapt_se(cutadapt_se_data):
-    df = odo(str(cutadapt_se_data), DataFrame)
-    assert df.loc["Reads with adapters"]["value"] == 3
-
-def test_cutadapt_pe(cutadapt_pe_data):
-    df = odo(str(cutadapt_pe_data), DataFrame)
-    assert df.loc["Read 1 with adapter"]["value"] == 3
-    assert list(df.loc["Read 1"]["value"]) == [1900, 1868]
-
+def test_cutadapt(cutadapt_metrics):
+    module, command, version, end, pdir = cutadapt_metrics
+    fn = str(pdir.join("cutadapt_metrics.txt"))
+    df = odo(cutadapt.resource_cutadapt_metrics(fn), DataFrame, foo="bar")
+    if end == "se":
+        assert df.loc["Reads with adapters"]["value"] == 792
+    elif end == "pe":
+        assert df.loc["Read 1 with adapter"]["value"] == 792
