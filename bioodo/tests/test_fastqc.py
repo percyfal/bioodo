88a0b242390cb05f2c477903dd85f35869cd11cf
Per Unneberg <per.unneberg@scilifelab.se>
2017-05-16 14:28:48 +0200

    Update fastqc to work with pytest_ngsfixtures

--- bioodo/tests/test_fastqc.py
+++ bioodo/tests/test_fastqc.py
@@ -3,32 +3,47 @@ import os
 import pytest
 from bioodo import fastqc, odo, DataFrame
 
-@pytest.fixture(scope="module")
-def fastqc_data(tmpdir_factory):
-    """Setup fastqc data"""
-    fn = tmpdir_factory.mktemp('fastqc').join("fastqc_data.txt")
-    fn.mksymlinkto(os.path.join(pytest.datadir, "fastqc", "fastqc_data.txt"))
-    return fn
+from pytest_ngsfixtures.config import application_fixtures
+import utils
+
+fixtures = application_fixtures(application="fastqc")
+fastqc_data = utils.fixture_factory(fixtures)
 
 
 def test_basic_statistics(fastqc_data):
-    df = odo(str(fastqc_data), DataFrame)
-    assert(list(df.index) == ['Filename', 'File type', 'Encoding', 'Total Sequences', 'Sequences flagged as poor quality', 'Sequence length', '%GC'])
-    assert(df.loc["Filename", "Value"] == "s1_1.fastq.gz")
+    module, command, version, end, pdir = fastqc_data
+    fn = str(pdir.join("medium_fastqc.zip"))
+    df = odo(fn, DataFrame)
+    major, minor, patch = version.split(".")
+    if int(minor) >= 11:
+        assert(list(df.index) == ['Filename', 'File type', 'Encoding', 'Total Sequences', 'Sequences flagged as poor quality', 'Sequence length', '%GC'])
+    else:
+        assert(list(df.index) == ['Filename', 'File type', 'Encoding', 'Total Sequences', 'Filtered Sequences', 'Sequence length', '%GC'])
+    assert(df.loc["Filename", "Value"] == "medium.bam")
 
 
 def test_summary(fastqc_data):
-    df = odo(str(fastqc_data), DataFrame, key="Summary")
+    module, command, version, end, pdir = fastqc_data
+    fn = str(pdir.join("medium_fastqc.zip"))
+    df = odo(fn, DataFrame, key="Summary")
     assert(df.loc['Basic_Statistics', 'Value'] == "pass")
 
 
 def test_wrong_key(fastqc_data):
+    module, command, version, end, pdir = fastqc_data
+    fn = str(pdir.join("medium_fastqc.zip"))
     with pytest.raises(KeyError):
-        df = odo(str(fastqc_data), DataFrame, key="foo")
+        df = odo(fn, DataFrame, key="foo")
 
 
 def test_per_base_sequence_quality(fastqc_data):
-    df = odo(str(fastqc_data),  DataFrame, key="Per_base_sequence_quality")
-    assert df.shape[0] == 43
+    module, command, version, end, pdir = fastqc_data
+    fn = str(pdir.join("medium_fastqc.zip"))
+    df = odo(fn,  DataFrame, key="Per_base_sequence_quality")
+    major, minor, patch = version.split(".")
+    if int(minor) <= 10:
+        assert df.shape[0] == 28
+    else:
+        assert df.shape[0] == 55
     assert df.shape[1] == 6
 
