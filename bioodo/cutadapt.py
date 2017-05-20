70dfb844bdc97caeb170fdcf2643866c55411b0e
Per Unneberg <per.unneberg@scilifelab.se>
2017-05-20 15:28:52 +0200

    Update cutadapt tests, add aggregation test

--- bioodo/cutadapt.py
+++ bioodo/cutadapt.py
@@ -1,7 +1,13 @@
 # Copyright (C) 2015 by Per Unneberg
 import re
 import pandas as pd
-from bioodo import resource, annotate_by_uri, DataFrame
+import logging
+import bioodo
+from bioodo import resource, annotate_by_uri, DataFrame, utils
+
+logger = logging.getLogger(__name__)
+config = bioodo.__RESOURCE_CONFIG__['cutadapt']
+
 
 # Potentially add regexp for adapter sections as these are repetitive
 adapter_re = re.compile(r'''
@@ -15,8 +21,10 @@ def _split_x(x, delim=":"):
     y = x.strip().split(delim)
     return [y[0], re_trim.sub("", y[1])]
 
+
 # For now only return the summary section
-@resource.register('.+\.cutadapt_metrics')
+@resource.register(config['metrics']['pattern'],
+                   priority=config['metrics']['priority'])
 @annotate_by_uri
 def resource_cutadapt_metrics(uri, **kwargs):
     with open(uri) as fh:
@@ -27,3 +35,21 @@ def resource_cutadapt_metrics(uri, **kwargs):
     df["value"] = pd.to_numeric(df["value"])
     return df
 
+
+# Aggregation function
+def aggregate(infiles, outfile=None, regex=None, **kwargs):
+    """Aggregate individual cutadapt reports to one output file
+
+    Params:
+      infiles (list): list of input files
+      outfile (str): csv output file name
+      regex (str): regular expression pattern to parse input file names
+      kwargs (dict): keyword arguments
+
+    """
+    logger.debug("Aggregating cutadapt infiles {} in cutadapt aggregate".format(",".join(infiles)))
+    df = utils.aggregate_files(infiles, regex=regex, **kwargs)
+    if outfile:
+        df.to_csv(outfile)
+    else:
+        return df
--- bioodo/tests/test_cutadapt.py
+++ bioodo/tests/test_cutadapt.py
@@ -1,18 +1,26 @@
 # Copyright (C) 2015 by Per Unneberg
 from bioodo import cutadapt, odo, DataFrame
-
 from pytest_ngsfixtures.config import application_fixtures
 import utils
 
 fixtures = application_fixtures(application="cutadapt")
 cutadapt_metrics = utils.fixture_factory(fixtures)
+cutadapt_aggregate_data = utils.aggregation_fixture_factory(
+    [x for x in fixtures], 2)
 
 
 def test_cutadapt(cutadapt_metrics):
     module, command, version, end, pdir = cutadapt_metrics
     fn = str(pdir.join("cutadapt_metrics.txt"))
-    df = odo(cutadapt.resource_cutadapt_metrics(fn), DataFrame, foo="bar")
+    df = odo(fn, DataFrame)
     if end == "se":
         assert df.loc["Reads with adapters"]["value"] == 792
     elif end == "pe":
         assert df.loc["Read 1 with adapter"]["value"] == 792
+
+
+def test_cutadapt_aggregate(cutadapt_aggregate_data):
+    module, command, version, end, pdir = cutadapt_aggregate_data
+    df = cutadapt.aggregate([str(x) for x in pdir.listdir() if not x.basename.startswith("cutadapt")],
+                            regex=".*(?P<repeat>[0-9]+)_cutadapt_metrics.txt")
+    assert sorted(list(df["repeat"].unique())) == ['0', '1']
