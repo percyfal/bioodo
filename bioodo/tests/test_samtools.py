9988e0f2421f2288fb64fe22caf542ac6bb8216e
Per Unneberg <per.unneberg@scilifelab.se>
2017-05-23 15:45:49 +0200

    Fix failing test comparisons

--- bioodo/tests/test_samtools.py
+++ bioodo/tests/test_samtools.py
@@ -1,6 +1,4 @@
 # Copyright (C) 2015 by Per Unneberg
-import os
-import pytest
 from bioodo import samtools, odo, DataFrame
 from pytest_ngsfixtures.config import application_fixtures
 import utils
@@ -12,24 +10,27 @@ samtools_stats = utils.fixture_factory(stat_fixtures)
 
 
 def test_basic_statistics(samtools_stats):
+    _stats = {'1.2': {'se': 60037, 'pe': 120110},
+              '1.3.1': {'se': 60000, 'pe': 120000}}
     module, command, version, end, pdir = samtools_stats
     fn = str(pdir.join("medium.stats.txt"))
     df = odo(samtools.resource_samtools_stats(fn), DataFrame)
     assert (list(df.index)[0] == 'raw total sequences')
-    n = 60000 if end == "se" else 120000
-    assert(df.loc["sequences", "value"] == n)
+    assert(df.loc["sequences", "value"] == _stats[version][end])
 
 
-_gcc_stats = {'1.2': {'se': 30.12, 'pe': 30.21}, '1.3.1': {'se': 30.19, 'pe': 30.27}}
 def test_GCC(samtools_stats):
+    _gcc_stats = {'1.2': {'se': 30.12, 'pe': 30.21},
+                  '1.3.1': {'se': 30.19, 'pe': 30.27}}
     module, command, version, end, pdir = samtools_stats
     fn = str(pdir.join("medium.stats.txt"))
     df = odo(samtools.resource_samtools_stats(fn, key="GCC"), DataFrame)
     assert (df.loc[1]["A"] == _gcc_stats[version][end])
 
 
-_ffq_stats = {'1.2': {'se': 27624, 'pe': 27630}, '1.3.1': {'se': 27598, 'pe': 27598}}    
 def test_FFQ(samtools_stats):
+    _ffq_stats = {'1.2': {'se': 27624, 'pe': 27630},
+                  '1.3.1': {'se': 27598, 'pe': 27598}}
     module, command, version, end, pdir = samtools_stats
     fn = str(pdir.join("medium.stats.txt"))
     df = odo(samtools.resource_samtools_stats(fn, key="FFQ"), DataFrame)
