fdc68d8e4a7ddd078413802829c7a7129629e444
Per Unneberg <per.unneberg@scilifelab.se>
2017-10-20 09:58:24 +0200

    Add regex functionality to annotate_by_uri
    Fixes #13

--- bioodo/pandas.py
+++ bioodo/pandas.py
@@ -1,6 +1,10 @@
 # Copyright (C) 2015 by Per Unneberg
+import logging
+import re
 import pandas as pd
 from blaze import append, DataFrame
+
+logger = logging.getLogger(__name__)
 
 
 @append.register(DataFrame, DataFrame)
@@ -10,14 +14,44 @@ def append_dataframe_to_dataframe(tgt, src, **kw):
 
 
 def annotate_by_uri(func):
+    """Decorator function annotate_by_uri.
+
+    Wrap function with wrapper that will optionally add an annotation
+    to the data frame based on the uri. This can be particularly
+    useful when merging several files and the data provenance needs to
+    be tracked.
+
+    Params:
+      annotate (bool): whether or not to annotate data frame
+      annotation_fn (function): use custom function to annotate data frame
+      regex (str): use regular expression to annotate data frame
+
+    """
+    def default_annotation_fn(df, uri, **kwargs):
+        logger.debug("Annotating dataframe with uri {}".format(uri))
+        df['uri'] = uri
+
+    def regex_annotation_fn(df, uri, regex, **kwargs):
+        logger.debug("Searching uri {} with regex {}".format(uri, regex))
+        m = re.search(regex, uri)
+        if m:
+            logger.debug("adding columns {}".format(",".join(["{}={}".format(k, v) for k, v in m.groupdict().items()])))
+            for k, v in m.groupdict().items():
+                df[k] = v
+
+
     def wrap(uri, **kwargs):
         df = func(uri, **kwargs)
-        if not kwargs.get('annotate', False):
-            return df
+        regex = kwargs.get('regex', None)
         annotation_fn = kwargs.get('annotation_fn', None)
-        if annotation_fn is not None:
-            df = annotation_fn(df, uri, **kwargs)
+        annotate_default = regex is not None or annotation_fn is not None
+        annotate = kwargs.get('annotate', annotate_default)
+        if not annotate:
+            return df
+        if regex is not None:
+            annotation_fn = regex_annotation_fn
         else:
-            df['uri'] = uri
+            annotation_fn = kwargs.get('annotation_fn', default_annotation_fn)
+        annotation_fn(df, uri, **kwargs)
         return df
     return wrap
